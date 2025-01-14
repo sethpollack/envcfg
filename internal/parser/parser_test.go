55a17671e7e7ca7524e04e3bf639b1de07948840
Seth Pollack <seth@sethpollack.net>
2025-01-13 20:34:27 -0500

    use maps

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -10,255 +10,213 @@ import (
 )
 
 func TestParseKind(t *testing.T) {
-	tt := []struct {
-		name        string
+	tt := map[string]struct {
 		kind        reflect.Kind
 		value       string
 		expected    any
 		expectedErr bool
 	}{
-		{
-			name:     "string",
+		"string": {
 			kind:     reflect.String,
 			value:    "hello",
 			expected: "hello",
 		},
-		{
-			name:     "empty string",
+		"empty string": {
 			kind:     reflect.String,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:     "int",
+		"int": {
 			kind:     reflect.Int,
 			value:    "42",
 			expected: int(42),
 		},
-		{
-			name:     "empty int string",
+		"empty int string": {
 			kind:     reflect.Int,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid int",
+		"invalid int": {
 			kind:        reflect.Int,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "int8",
+		"int8": {
 			kind:     reflect.Int8,
 			value:    "42",
 			expected: int8(42),
 		},
-		{
-			name:     "empty int8 string",
+		"empty int8 string": {
 			kind:     reflect.Int8,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid int8",
+		"invalid int8": {
 			kind:        reflect.Int8,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "int16",
+		"int16": {
 			kind:     reflect.Int16,
 			value:    "42",
 			expected: int16(42),
 		},
-		{
-			name:     "empty int16 string",
+		"empty int16 string": {
 			kind:     reflect.Int16,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid int16",
+		"invalid int16": {
 			kind:        reflect.Int16,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "int32",
+		"int32": {
 			kind:     reflect.Int32,
 			value:    "42",
 			expected: int32(42),
 		},
-		{
-			name:     "empty int32 string",
+		"empty int32 string": {
 			kind:     reflect.Int32,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid int32",
+		"invalid int32": {
 			kind:        reflect.Int32,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "int64",
+		"int64": {
 			kind:     reflect.Int64,
 			value:    "42",
 			expected: int64(42),
 		},
-		{
-			name:     "empty int64 string",
+		"empty int64 string": {
 			kind:     reflect.Int64,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid int64",
+		"invalid int64": {
 			kind:        reflect.Int64,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "uint",
+		"uint": {
 			kind:     reflect.Uint,
 			value:    "42",
 			expected: uint(42),
 		},
-		{
-			name:     "empty uint string",
+		"empty uint string": {
 			kind:     reflect.Uint,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid uint",
+		"invalid uint": {
 			kind:        reflect.Uint,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "uint8",
+		"uint8": {
 			kind:     reflect.Uint8,
 			value:    "42",
 			expected: uint8(42),
 		},
-		{
-			name:     "empty uint8 string",
+		"empty uint8 string": {
 			kind:     reflect.Uint8,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid uint8",
+		"invalid uint8": {
 			kind:        reflect.Uint8,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "uint16",
+		"uint16": {
 			kind:     reflect.Uint16,
 			value:    "42",
 			expected: uint16(42),
 		},
-		{
-			name:     "empty uint16 string",
+		"empty uint16 string": {
 			kind:     reflect.Uint16,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid uint16",
+		"invalid uint16": {
 			kind:        reflect.Uint16,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "uint32",
+		"uint32": {
 			kind:     reflect.Uint32,
 			value:    "42",
 			expected: uint32(42),
 		},
-		{
-			name:     "empty uint32 string",
+		"empty uint32 string": {
 			kind:     reflect.Uint32,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid uint32",
+		"invalid uint32": {
 			kind:        reflect.Uint32,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "uint64",
+		"uint64": {
 			kind:     reflect.Uint64,
 			value:    "42",
 			expected: uint64(42),
 		},
-		{
-			name:     "empty uint64 string",
+		"empty uint64 string": {
 			kind:     reflect.Uint64,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid uint64",
+		"invalid uint64": {
 			kind:        reflect.Uint64,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "float32",
+		"float32": {
 			kind:     reflect.Float32,
 			value:    "3.14",
 			expected: float32(3.14),
 		},
-		{
-			name:     "empty float32 string",
+		"empty float32 string": {
 			kind:     reflect.Float32,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid float32",
+		"invalid float32": {
 			kind:        reflect.Float32,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "float64",
+		"float64": {
 			kind:     reflect.Float64,
 			value:    "3.14",
 			expected: float64(3.14),
 		},
-		{
-			name:     "empty float64 string",
+		"empty float64 string": {
 			kind:     reflect.Float64,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid float64",
+		"invalid float64": {
 			kind:        reflect.Float64,
 			value:       "invalid",
 			expectedErr: true,
 		},
-		{
-			name:     "bool",
+		"bool": {
 			kind:     reflect.Bool,
 			value:    "true",
 			expected: true,
 		},
-		{
-			name:     "empty bool string",
+		"empty bool string": {
 			kind:     reflect.Bool,
 			value:    "",
 			expected: nil,
 		},
-		{
-			name:        "invalid bool",
+		"invalid bool": {
 			kind:        reflect.Bool,
 			value:       "invalid",
 			expectedErr: true,
@@ -267,8 +225,8 @@ func TestParseKind(t *testing.T) {
 
 	p := New()
 
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
 			newValue, _, err := p.ParseKind(tc.kind, tc.value)
 			if tc.expectedErr {
 				require.Error(t, err)
@@ -281,21 +239,18 @@ func TestParseKind(t *testing.T) {
 }
 
 func TestParseType(t *testing.T) {
-	tt := []struct {
-		name        string
+	tt := map[string]struct {
 		typ         reflect.Type
 		value       string
 		expected    any
 		expectedErr bool
 	}{
-		{
-			name:     "duration",
+		"duration": {
 			typ:      reflect.TypeOf(time.Nanosecond),
 			value:    "1s",
 			expected: time.Second,
 		},
-		{
-			name:     "empty duration",
+		"empty duration": {
 			typ:      reflect.TypeOf(time.Nanosecond),
 			value:    "",
 			expected: nil,
@@ -304,8 +259,8 @@ func TestParseType(t *testing.T) {
 
 	p := New()
 
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
 			newValue, _, err := p.ParseType(tc.typ, tc.value)
 			if tc.expectedErr {
 				require.Error(t, err)
